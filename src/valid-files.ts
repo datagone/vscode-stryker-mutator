72ef36f9f0ff844a5c957b728aaab0935fef9e23
alexchapar <alexchapar@users.noreply.github.com>
2023-05-27 23:56:53 -0400

    fix(context menu): change titles in the menu and show only on specified files (#44)
    ## 🚀 Changes proposed by this PR
    
    Change the titles in the menu to "Trigger mutation test on..." to be
    more suitable for the action that it does
    Hide the actions on file/selection if the file is within a test project 
    Update the readme
    
    ### 🧰 Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue).
    - [ ] New feature (non-breaking change which adds functionality).
    - [ ] Breaking change (fix or feature that causes existing functionality
    to not work as expected).
    - [x] This change requires (or is) a documentation update.
    
    ### 📝 Notes to reviewer
    
    <!--
    If needed, leave any special pointers for reviewing or testing your PR.
    If necessary, include things like screenshots (where beneficial), to
    help demonstrate the changes.
    -->
    
    ### 🤝 Requirements
    
    - [x] ✍ I have read and agree to this projects [Code of
    Conduct](../../blob/main/.github/CODE_OF_CONDUCT.md).
    - [x] ✍ I have read and agree to this projects [Contribution
    Guidelines](../../blob/main/.github/CONTRIBUTING.md).
    - [x] ✍ I have read and agree to the [Developer Certificate of
    Origin](https://developercertificate.org/).
    - [x] 🔎 I have performed a self-review of my own changes.
    - [x] 🎨 My changes follow the style guidelines of this project.
    <!-- Include the below if this is a code change, if just documentation,
    ❌ remove this section. -->
    - [x] 💬 My changes as properly commented, primarily for
    hard-to-understand areas.
    - [x] 📝 I have made corresponding changes to the documentation.
    - [x] 🧪 I have included tests (if necessary) for this change.

--- src/valid-files.ts
+++ src/valid-files.ts
@@ -5,4 +5,4 @@ const IS_TEST_FILE_REGEX = new RegExp(/tests?[\w-.\\/]*\.(cs)$/, 'i');
 export const isTestFile = (file: string) => IS_TEST_FILE_REGEX.test(file);
 
 export const showInvalidFileMessage = async () =>
-  await vscode.window.showErrorMessage('Cannot run Stryker to mutate a test files');
+  await vscode.window.showErrorMessage('Stryker.NET: Cannot run Stryker to mutate a test files');
